703d0c29c959257b87e19b0fc14d9a0120cf55d6
cbotek <botekchristophe@gmail.com>
2021-10-27 16:20:48 -0400

    UI-32 generate yml file with helm

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -7,4 +7,4 @@ RELEASE_NAME=airflow
 helm repo add apache-airflow https://airflow.apache.org
 helm install $RELEASE_NAME apache-airflow/airflow \
 --namespace airflow \
---values config.yaml
+--values ../values.yaml
