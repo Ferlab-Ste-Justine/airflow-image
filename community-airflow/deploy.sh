085aa767e114aecd36c79d7a195bcd0a7164d13e
cbotek <cbotek@ferlab.bio>
2022-05-27 14:07:52 -0700

    remove unused secrets

--- community-airflow/deploy.sh
+++
@@ -1,5 +0,0 @@
-helm install --namespace=airflow\
---timeout=10m0s\
---values=/home/shell/helm/values-airflow-8.5.3.yaml\
---version=8.5.3\
---wait=true community-airflow /home/shell/helm/airflow-8.5.3.tgz
